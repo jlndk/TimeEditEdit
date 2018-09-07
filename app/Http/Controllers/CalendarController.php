9d770d849bde53e706942923319e4d51ffebe98f
Jonas Lindenskov Nielsen <lindenskov.nielsen@gmail.com>
2018-09-07 11:37:41 +0200

    Implemented caching (#6)
    Fixes #5

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -2,19 +2,35 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Calendar\Calendar;
 use App\Calendar\EventTransformer;
+use \App;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Log;
 
 class CalendarController extends Controller
 {
     function show($calid, Request $request) {
+        $cachedCalendar = Cache::get($calid);
+
+        /**
+         * If we have a cached version of the calendar and we're not in
+         * development mode we return that instead of fetching it from TimeEdit.
+         *
+         */
+        if($cachedCalendar && !App::environment('local')) {
+            return $cachedCalendar;
+        }
+
         $calendar = new Calendar("https://cloud.timeedit.net/itu/web/public/$calid.ics");
 
         foreach($calendar->events as $i => $event) {
             $et = new EventTransformer($event);
             $calendar->events[$i] = $et->transform();
         }
+
+        Cache::put($calid, $calendar, 5);
 
         return $calendar;
     }
