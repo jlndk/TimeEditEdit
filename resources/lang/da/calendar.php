95ddae0d3f9532473b9d84eea37611a630ea5ba1
Jonas Lindenskov Nielsen <lindenskov.nielsen@gmail.com>
2020-07-01 09:36:00 +0200

    Added formattting for reexam submissions

--- resources/lang/da/calendar.php
+++ resources/lang/da/calendar.php
@@ -9,6 +9,7 @@ return [
         'ordinary exam' => 'Eksamen',
         'reexam' => 'Reeksamen',
         'reexam - on premises' => 'Reeksamen',
+        'reexam - submission' => 'Reeksamen aflevering'
     ],
 
     'lectors' => 'Lektor|Lektorer',
